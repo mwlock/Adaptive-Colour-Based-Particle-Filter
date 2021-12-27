4f256de23813238c82d8e25798c7462075116e86
Miguel Garcie Naude <miguelito2384@gmail.com>
2021-12-27 12:02:57 +0100

    pig video

---
+++ src/matlab/pf_functions/weights.m
@@ -0,0 +1,16 @@
+% This file is to return the calculated weights of the particles
+%
+% "3D Trajectory Prediction of Basketball Shot Using Filtering Techniques
+% and Computer Vision" project. The project is a self picked topic for implementation
+% in the Appied Estimation course at the KTH Royal Institute of Technology in 2021.
+%
+% Authors : 
+% Matthew William Lock (mwlock@kth.se)
+% Miguel Garcia Naude (magn2@kth.se)
+
+% This function calculates the weight assigned to each particle
+% observation likelihood
+%           image                
+%           binaryImage
+% Outputs: 
+%           histogram            3X8    
